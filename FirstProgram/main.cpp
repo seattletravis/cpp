c1344cd17abd8c523e867080c73364af3c18a75b
Travis Lamberte <travis@lamberte.com>
2024-05-27 17:02:36 -0700

    add std in to get number

--- FirstProgram/main.cpp
+++ FirstProgram/main.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 
+int main() 
+{
+  int favorite_number;
+  std::cout << "What is your favoritate Number? (Between 1 and 100) ";
+
+  std::cin >> favorite_number;
+
+  std::cout << "Your favorite Number is ";
+
+  std::cout << 
+  return 0;
+}
