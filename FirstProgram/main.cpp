2706b336c7d4b3ec815648183ca975d797de4eb0
Travis Lamberte <travis@lamberte.com>
2024-05-30 06:36:43 -0700

    cleanup

--- FirstProgram/main.cpp
+++ FirstProgram/main.cpp
@@ -4,10 +4,7 @@ int main()
 {
   int favorite_number;
   std::cout << "What is your favoritate Number? (Between 1 and 100) ";
-
   std::cin >> favorite_number;
-
   std::cout << "Your favorite Number is..., I forgot!" << std::endl;
-
   return 0;
 }
