891d404b776c887f8a09d6cf2a13b3b356c5a1c9
Travis Lamberte <travis@lamberte.com>
2024-05-31 07:41:06 -0700

    refactor

--- FirstProgram/main.cpp
+++ FirstProgram/main.cpp
@@ -1,15 +1,66 @@
 #include <iostream>
 
-int main() {
+int main(){
   int favorite_number;
-  std::cout << "What's your favorite number ";
+  std::cout << "What is your favorite number ";
   std::cin >> favorite_number;
-  std::cout << "Amazing!! That's my favorite number too!" << std::endl;
-  std::cout << "No really!!, ";
+  std::cout << "Amazing that is my favorite number too!!!" << std::endl;
+  std::cout << "No really! ";
   std::cout << favorite_number;
-  std::cout << ", is my favorite number!";
+  std::cout << ", is my favorite!" << std::endl;
   return 0;
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int main() {
+//   int favorite_number;
+//   std::cout << "What's your favorite number ";
+//   std::cin >> favorite_number;
+//   std::cout << "Amazing!! That's my favorite number too!" << std::endl;
+//   std::cout << "No really!!, ";
+//   std::cout << favorite_number;
+//   std::cout << ", is my favorite number!";
+//   return 0;
+
+// }
 
 
