ae8ce1a1bf401adc478002355340c27286225cfa
Travis Lamberte <travis@lamberte.com>
2024-06-02 07:21:54 -0700

    prepare to add new code snippet

--- BasicIO/src/main.cpp
+++ BasicIO/src/main.cpp
@@ -6,16 +6,28 @@ using namespace std;
 
 int main() 
 {
-  cout << "Hello World" << endl;
+  // cout << "Hello World" << endl;
 
-  cout << "Hello";
-  cout << "World" << endl;
+  // cout << "Hello";
+  // cout << "World" << endl;
 
-  cout << "Hello World" << endl;
-  cout << "Hello" << " world!" << endl;
-  cout << "Hello" << " world!\n";
-  cout << endl;
-  cout << "Hello\nOut\nThere\n";
+  // cout << "Hello World" << endl;
+  // cout << "Hello" << " world!" << endl;
+  // cout << "Hello" << " world!\n";
+  // cout << endl;
+  // cout << "Hello\nOut\nThere\n";
+
+  // int num1;
+  // int num2;
+  // double num3;
+
+  // cout << "Enter a number: ";
+  // cin >> num1;
+  // cout << "You entered: " << num1 << endl;
+  
+  
+
+
 
   return 0;
 }
