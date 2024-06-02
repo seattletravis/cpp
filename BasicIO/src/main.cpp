18b7c0beca4003a836cbfe165731da4a56ac5b67
Travis Lamberte <travis@lamberte.com>
2024-06-02 07:25:03 -0700

    write snippet

--- BasicIO/src/main.cpp
+++ BasicIO/src/main.cpp
@@ -25,7 +25,15 @@ int main()
   // cin >> num1;
   // cout << "You entered: " << num1 << endl;
   
-  
+  int num1;
+  cout << "Enter a first integer: ";
+  cin >> num1;
+
+  int num2;
+  cout << "Enter a second integer: ";
+  cin >> num2;
+
+  cout << "You entered " << num1 << " and " << num2 << endl;
 
 
 
