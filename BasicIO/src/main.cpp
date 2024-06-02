871fcc88130423bd9ff8bb9bd7709cf856471a24
Travis Lamberte <travis@lamberte.com>
2024-06-02 07:39:38 -0700

    double

--- BasicIO/src/main.cpp
+++ BasicIO/src/main.cpp
@@ -17,33 +17,35 @@ int main()
   // cout << endl;
   // cout << "Hello\nOut\nThere\n";
 
-  // int num1;
-  // int num2;
-  // double num3;
+  int num1;
+  int num2;
+  double num3;
+
+
 
   // cout << "Enter a number: ";
   // cin >> num1;
   // cout << "You entered: " << num1 << endl;
   
-  // int num1;
-  // int num2;
+
 
   // cout << "Enter a first integer: ";
   // cin >> num1;
-
   // cout << "Enter a second integer: ";
   // cin >> num2;
-
   // cout << "You entered " << num1 << " and " << num2 << endl;
 
 
-  int num1;
-  int num2;
 
-  cout << "Enter 2 integers seperated by a space: ";
-  cin >> num1 >> num2;
-  cout << "You entered " << num1 << " and " << num2 << endl;
+  // cout << "Enter 2 integers seperated by a space: ";
+  // cin >> num1 >> num2;
+  // cout << "You entered " << num1 << " and " << num2 << endl;
 
+
+
+  cout << "Enter a double: ";
+  cin >> num3;
+  cout << "You entered: " << num3;
 
 
   return 0;
