b6fb018b21123b4fe459d5a94992cb78237b35ef
Travis Lamberte <travis@lamberte.com>
2024-06-01 18:05:13 -0700

    add simple function to add 2 integers

--- Project_1/SecondProgram/main.cpp
+++ Project_1/SecondProgram/main.cpp
@@ -2,6 +2,11 @@
 
 // This is a comment
 
+int add_ints(int a, int b)
+{
+  return a + b;
+}
+
 int main()
 {
   std::string favorite_color;
@@ -11,6 +16,15 @@ int main()
   std::cout << "No really ";
   std::cout << favorite_color;
   std::cout << ", is my favorite color!!!" << std::endl;
+
+  int a = 100;
+  int b = 100;
+
+  int result = add_ints(a, b);
+
+  std::cout << "Also a + b is equal to: ";
+  std::cout << result;
+
 
   return 0;
 }
