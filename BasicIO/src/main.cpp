6ea6df2e8a90015f0eea1dcd56a717ff59bc6cd1
Travis Lamberte <travis@lamberte.com>
2024-06-02 06:50:52 -0700

    created new project to practice Basic IO

---
+++ BasicIO/src/main.cpp
@@ -0,0 +1,20 @@
+// Basic I/O using cin and cout
+
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+  cout << "Hello World" << endl;
+
+  cout << "Hello";
+  cout << "World" << endl;
+
+  cout << "Hello World" << endl;
+  cout << "Hello" << " world!" << endl;
+  cout << "Hello" << " world!\n";
+  cout << "Hello\nOut\nThere\n";
+
+  return 0;
+}
