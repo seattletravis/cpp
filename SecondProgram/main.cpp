5a788dcedd16a8faa08946911317105771ae9795
Travis Lamberte <travis@lamberte.com>
2024-06-01 17:44:32 -0700

    write new project

---
+++ SecondProgram/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+  std::string favorite_color;
+  std::cout << "What is your favorite color?";
+  std::cin >> favorite_color;
+  std::cout << "That's my favorite color too!!!";
+  std::cout << "No really ";
+  std::cout << favorite_color;
+  std::cout << ", is my favorite color!!!" << std::endl;
+  return 0;
+}
