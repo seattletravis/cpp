12d7215b62c9fa070da86190387dfa427db00bc7
Travis Lamberte <travis@lamberte.com>
2024-06-03 10:50:39 -0700

    add Hello World project

---
+++ HelloWorld/hello_world.cpp
@@ -0,0 +1,11 @@
+// make sure you have a c++ compiler installed, I'm using minGW
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << "Hello World" << endl;
+	return 0;
+}
+
---
+++ Variables/Chars/char.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char first_letter {'T'};
+	cout << "Enter the first letter of your name: " << endl;
+	cin >> first_letter;
+	cout << "The first letter of your name is " << first_letter << "!" << endl;
+	
+	return 0;
+}
