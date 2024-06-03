250f3e4fcef110e6fb52db4dada3b623ea7ebdaa
Travis Lamberte <travis@lamberte.com>
2024-06-03 12:00:37 -0700

    add valuable comments

--- Variables/PrimitiveTypes/main.cpp
+++ Variables/PrimitiveTypes/main.cpp
@@ -7,18 +7,24 @@ int main(){
   char middle_initial {'J'};
   cout << "My middle initial is: " << middle_initial << endl;
 
+  // short int max = 32,767
   unsigned short int exam_score {55};
   cout << "My exam score is: " << exam_score << endl;
 
+  // int and long are the same max value for some reason
   int countries_represented {65};
   cout << "There are: " << countries_represented << " countries represented" << endl;
 
+  // long max = ~2.15 x 10^8 (double if unsigned)
   long florida_population {20'610'000};
   cout << "There are: " << florida_population << " people in Florida" << endl;
 
+  // long long max = ~9.22 x 10^18 (double if unsigned)
   long long earth_population {7'600'000'000};
   cout << "There are: " << earth_population << " people on planet earth!" << endl;
 
+  long long dist_to_centauri {9'461'000'000'000};
+  cout << "Distance to Alpha Centauri: " << dist_to_centauri << " KM!" << endl;
 
   return 0;
 }
