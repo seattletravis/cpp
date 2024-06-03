5a65ecc73a3fde1d95244e4d7420fc242a1a9ce4
Travis Lamberte <travis@lamberte.com>
2024-06-03 11:17:51 -0700

    changes

--- Variables/PrimitiveTypes/main.cpp
+++ Variables/PrimitiveTypes/main.cpp
@@ -4,8 +4,11 @@ using namespace std;
 
 int main(){
 
+  char middle_initial {'J'};
+  cout << "My middle initial is: " << middle_initial << endl;
+
   unsigned short int exam_score {55};
-  cout << "My exam score was: " << exam_score << endl;
+  cout << "My exam score is: " << exam_score << endl;
 
 
 
