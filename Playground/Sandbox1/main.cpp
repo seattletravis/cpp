fda1dcc4a569ae42a9d8e290070986f66a5da162
Travis Lamberte <travis@lamberte.com>
2024-06-03 15:25:07 -0700

    add sandbox

---
+++ Playground/Sandbox1/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    char A = '2';
+    int sum = A + 10;
+    std::cout<<"sum = "<<sum;
+    
+    int sum1 = 'A' + 10;
+    std::cout<<"\nsum1  = "<<sum1;
+    
+    char sum2 = 'A' + 10;
+    std::cout<<"\nsum2 = "<<sum2;
+    
+    char B = '2322';
+
+    int sum3 = B + 10;
+    std::cout<<"\nsum3 = "<<sum3;
+  return 0;
+}
