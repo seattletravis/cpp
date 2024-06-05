3d320b8a4a6aec898af16ce627736d84c11fc8a7
Travis Lamberte <travis@lamberte.com>
2024-06-05 09:24:36 -0700

    change code

--- CleaningExercise/main.cpp
+++ CleaningExercise/main.cpp
@@ -12,14 +12,17 @@ int main(){
   int num_room {2};
   const int per_room {30};
   const float tax_rate {0.06};
-  float tax {0};
   const string footer {"This estimate is valid for 30 days"};
+  float sub_total {0};
+  float tax {0};
+
   cout << title << endl;
   cout << "\nHow many rooms would you like cleaned: " << endl;
   cin >> num_room;
   cout << "Number of rooms: " << num_room << endl;
   cout << "Price per room: $" << per_room << endl;
-  cout << "Cost: $" << num_room * per_room << endl;
+  sub_total = num_room * per_room;
+  cout << "Subtotal: $" << sub_total << endl;
   tax = num_room * per_room * tax_rate;
   cout << "Tax: $" << tax << endl;
 
