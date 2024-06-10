cc7f2e50a86d78cdedac7a3248952877ba433cba
Travis Lamberte <travis@lamberte.com>
2024-06-10 08:31:43 -0700

    add test_scores array

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -15,4 +15,13 @@ int main(){
   cout << "and the low temp: " << hi_temps[2] << endl;
   cout << "all the temps " << hi_temps[1];
 
+  int test_scores[5];
+
+  cout << "\nFirst score at index 0: " << test_scores[0] << endl;
+  cout << "Second score at index 1: " << test_scores[1] << endl;
+  cout << "Third score at index 2: " << test_scores[2] << endl;
+  cout << "Fourth score at index 3: " << test_scores[3] << endl;
+  cout << "Fifth score at index 4: " << test_scores[4] << endl;
+
+
 }
