1a985639b7d166ac8bf12c6bcf9074ba39a4fa14
Travis Lamberte <travis@lamberte.com>
2024-06-11 06:05:26 -0700

    cout thresholds

--- Vectors/main.cpp
+++ Vectors/main.cpp
@@ -6,7 +6,11 @@ using namespace std;
 int main(){
 vector <int> thresholds {10,20,30,40,50,60,70};
 
-cin >> thresholds.at(0) >> thresholds.at(1) >> thresholds.at(2) >> thresholds.at(3) >> thresholds.at(4) >> thresholds.at(5);
+  cin >> thresholds.at(0) >> thresholds.at(1) >> thresholds.at(2) >> thresholds.at(3) >> thresholds.at(4) >> thresholds.at(5);
   
+  for (int i = 0; i < thresholds.size(); i++){
+    cout << thresholds[1];
+  }
+
   return 0;
 }
