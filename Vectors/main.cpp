1773565f8da674fa0a1f4cf606fbe35989391725
Travis Lamberte <travis@lamberte.com>
2024-06-11 06:10:01 -0700

    cout results

--- Vectors/main.cpp
+++ Vectors/main.cpp
@@ -4,13 +4,14 @@ using namespace std;
 
 
 int main(){
-vector <int> thresholds {10,20,30,40,50,60,70};
+vector <int> thresholds {10,20,30};
 
-  cin >> thresholds.at(0) >> thresholds.at(1) >> thresholds.at(2) >> thresholds.at(3) >> thresholds.at(4) >> thresholds.at(5);
-  
+  cin >> thresholds.at(0) >> thresholds.at(1) >> thresholds.at(2);
+
+  cout << thresholds.size() << endl;  
   for (int i = 0; i < thresholds.size(); i++){
-    cout << thresholds[1];
-  }
+    cout << thresholds[1] << " , " << endl;
+  };
 
   return 0;
 }
