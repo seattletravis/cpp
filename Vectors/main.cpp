984e625856da877c52b05a68df56e4da4df8aa5b
Travis Lamberte <travis@lamberte.com>
2024-06-12 07:22:48 -0700

    fix typo

--- Vectors/main.cpp
+++ Vectors/main.cpp
@@ -6,11 +6,12 @@ using namespace std;
 int main(){
 vector <int> thresholds {10,20,30};
 
-  cin >> thresholds.at(0) >> thresholds.at(1) >> thresholds.at(2);
+  thresholds.push_back(25);
 
   cout << thresholds.size() << endl;  
+
   for (int i = 0; i < thresholds.size(); i++){
-    cout << thresholds[1] << " , " << endl;
+    cout << thresholds[i] << ", " << endl;
   };
 
   return 0;
