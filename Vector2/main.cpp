cead45a6fc353c4e3af43d48ff737da21981255c
Travis Lamberte <travis@lamberte.com>
2024-06-12 07:28:16 -0700

    add vector2 project

---
+++ Vector2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  vector <char> vowels {'a', 'e', 'i', 'o', 'u'};
+
+  cout << vowels[0] << endl;
+  cout << vowels [4] << endl;
+
+  
+}
