ce2b98c354a37e2e07cbc23773c0b0c8115866d7
Travis Lamberte <travis@lamberte.com>
2024-06-19 13:51:14 -0700

    add dir

--- Expressions/main.cpp
+++ Expressions/main.cpp
@@ -4,7 +4,15 @@ using namespace std;
 
 int main(){
   
-  
+  int int1 {5};
+  int int2 {10};
+  int int3 {20};
+
+  cout << int1 << " " << int2 << " " << int3 << endl;
+  int sum {0};
+  sum = int1 + int2 + int3;
+
+  cout << sum << endl;
 
   
   return 0;
